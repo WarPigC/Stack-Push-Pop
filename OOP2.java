111f5768f2731629850e7901a1f6ccace71da038
WarPigC <85150042+WarPigC@users.noreply.github.com>
2023-07-02 22:12:48 +0530

    __

--- OOP2.java
+++ OOP2.java
@@ -1,5 +1,5 @@
 class Stack{
-    private int ar[] = {0,1,2,3,4,5,6,7,8,9}; // Only Methods can display/edit it
+    private int ar[] = {0,1,2,3,4,5,6,7,8,9}; 
     void display(){
         String y = "";
         for (int x:ar){
@@ -40,51 +40,21 @@ class Stack{
             System.out.println("Array Empty");
         }
     }
-    private int[] Ret(){          // It is private but can be used by the class
-        return ar;
-    }
-
-    void Compare(Stack ob){
-        int arr[] = new int [10];
-        int arr2[] = new int [10];
-        arr = Ret();
-        arr2 = ob.Ret();                                              // Usage of private
-        if (arr[0] == arr2[0]) System.out.println("ar[0] are the same");
-    }
-}
-
-class Create{
-    Stack create(){
-        return new Stack();
-    }
 }
 
 class OOP2 {
     public static void main(String args[]){
         Stack d = new Stack();
         d.display();
-        // removes 11 numbers
-        for (int i = 0;i < 11;i++){
+        // removes 10 numbers
+        for (int i = 0;i < 10;i++){     
             d.pop();
         }
         d.display();
-        // adds 11 numbers 
-        for (int i = 0;i < 11;i++){
+        // adds 10 numbers 
+        for (int i = 0;i < 10;i++){     
             d.push(i);
         }
         d.display(); 
-
-        Stack a = new Stack();
-
-        d.Compare(a);
-
-        //creating a new object from a class returning an object
-
-        Create c = new Create();    // class is initialized
-        Stack stack = c.create();   // stack is assigned the object
-
-        // displaying new stack object made
-
-        stack.display();
     }
 }
